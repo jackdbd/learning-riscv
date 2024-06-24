e464702cc27d0b0fe1cfaa2d7194dcc9da8df9a2
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-06-24 12:47:08 +0200

    add references in README

--- README.md
+++ README.md
@@ -61,7 +61,8 @@ Reference: Table RISC-V calling convention register usage in chapter [Calling Co
 - [RISC-V Reference (PDF)](https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_CARD.pdf)
 - [RISC-V Online Assembler](https://riscvasm.lucasteske.dev/)
 - [RISC-V System call table](https://jborza.com/post/2021-05-11-riscv-linux-syscalls/)
-- [The Magic of RISC-V Vector Processing (LaurieWired)](https://youtu.be/Ozj_xU0rSyY?si=Ba6-AvblLUVcKzMW)
+- [The Magic of RISC-V Vector Processing (LaurieWired - YouTube)](https://youtu.be/Ozj_xU0rSyY?si=Ba6-AvblLUVcKzMW)
 - [Understanding RISC-V Calling Convention](https://www2.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_CALL.pdf)
 - [Syscalls, exceptions, and
 interrupts, …oh my!](https://www.cs.cornell.edu/courses/cs3410/2019sp/schedule/slides/14-ecf-pre.pdf)
+- [LearnRISC-V - YouTube](https://www.youtube.com/@LearnRISCV/videos)
--- os-no-libc/exit-code/exit-code.s
+++ os-no-libc/exit-code/exit-code.s
@@ -1,18 +1,18 @@
 .section .text
 .globl _start
-.equ EXIT, 93
 
 _start:
-    # add 7 to the value of the x0 register (which on RISC-V is constantly at 0)
-    # and store the sum in x10 (so basically set x10 to 7)
+    # Add 7 to the value of the x0 register (which on RISC-V is constantly at 0)
+    # and store the sum in x10 (so basically set x10 to 7).
     addi x10, x0,  7
 
-    # set x17 to 93. 93 is the Linux syscall exit. The x17 user-level base
-    # integer register correspondes to a7 in the ABI.
-    addi x17, x0, EXIT
+    # Set x17 to 93. 93 is the Linux syscall exit. The x17 user-level base
+    # integer register corresponds to a7 in the ABI.
+    addi x17, x0, 93
     # The following instruction would have the same effect as `addi x17, x0, 93`,
     # but `li` is a pseudo-instruction, while `addi` is an actual instruction.
     # li a7, 93      
 
-    # environment call (make a system call to the operating system)
+    # Make a request to the supporting execution environment (the execution
+    # environment is most likely an operating system).
     ecall
